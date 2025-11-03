24f630950d6cdd7e458349504d71defb0f27a492
iam-qwerty <eabiodun45@gmail.com>
2025-11-03 09:28:43 +0100

    Google Analytics integration

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,7 +3,7 @@ import { Montserrat } from 'next/font/google'
 import './globals.css'
 import { Navigation } from '@/components/layout/Navigation'
 import Footer from '@/components/layout/Footer'
-
+import Script from 'next/script'
 // Font
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -49,6 +49,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Load Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LGQ7ZN8Y91"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LGQ7ZN8Y91', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+      </head>
       <body className={`${montserrat.className} bg-[#0a0a0a] text-white min-h-screen overflow-x-hidden`}>
         <Navigation />
         <main className="container mx-auto px-4 pt-24">
