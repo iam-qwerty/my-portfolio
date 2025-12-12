b5155e4fa7a232d069b97bfe5cec885f69142481
iam-qwerty <eabiodun45@gmail.com>
2025-12-12 18:45:24 +0100

    ...

--- frontend/components/sections/Hero.tsx
+++ frontend/components/sections/Hero.tsx
@@ -28,7 +28,7 @@ export default function HeroSection() {
           transition={{ duration: 0.5, delay: 0.2 }}
           className="text-base md:text-xl mb-8 leading-relaxed"
         >
-          Your friendly neighborhood front-end developer. <br />
+          Your friendly neighborhood Web developer. <br />
           I build Beautiful, Sleek, and Modern Websites.
         </motion.p>
         <motion.div
